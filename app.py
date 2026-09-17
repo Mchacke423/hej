6bec47d1de56750de196c7a3c406b36f7a4f8bd1
Mathias <mchacke@gmail.com>
2026-09-17 10:48:54 +0200

    Foerste version

---
+++ app.py
@@ -0,0 +1,11 @@
+﻿from flask import Flask
+from datetime import date
+
+app = Flask(__name__)
+
+@app.route("/")
+def forside():
+    return f"<h1>Hej Mathias</h1><p>I dag er det {date.today()}</p>"
+
+if __name__ == "__main__":
+    app.run(debug=True)
