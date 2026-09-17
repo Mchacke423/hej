eff104d4838d6a116a3e9182582f64ca3e1ae701
Mathias <mchacke@gmail.com>
2026-09-17 11:56:03 +0200

    Ny tekst

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 ﻿from flask import Flask
 from datetime import date
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def forside():
-    return f"<h1>Hej Mathias</h1><p>I dag er det {date.today()}</p>"
-
-import os
+    return f"<h1>Hej fra internettet</h1><p>I dag er det {date.today()}</p>"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
